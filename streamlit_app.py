9997a5d2438e62707648abfc43679c882678fd51
Srinivas-GH <LOGSRINIVAS@GMAIL.COM>
2025-03-14 23:26:47 +0530

    Update streamlit_app.py
    code changes to make it compatible with SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -21,7 +20,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on your Smoothie:")
 st.write("The Name on your Smoothie will be", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
